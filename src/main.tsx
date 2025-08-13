a142d86ab5706476c06eb06c45eec165d7c2ecdb
Vritika <vritikajangir2003@gmail.com>
2025-08-13 22:59:54 +0530

    correct import

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AppProvider } from "./context/AppContext";
-import { ThemeProvider } from "./context/ThemeContext"; 
-import "./index.css";
+import { ThemeProvider } from "./context/ThemeContext"; // adjust path if needed
+import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
